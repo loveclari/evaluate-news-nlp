56097345fea6a197e5bc532f38a70aa1e6e27ae5
loveclari <clarissa.celestino@gmail.comm>
2021-06-17 16:49:53 -0700

    adding test jest

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { checkForUrl } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
