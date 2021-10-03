29d29f7f4f837c054aa063a0928fc039ceeedc06
loveclari <clarissa.celestino@gmail.comm>
2021-10-03 10:29:28 -0700

    adding cors and fixing response

--- src/server/index.js
+++ src/server/index.js
@@ -38,19 +38,19 @@ const server = app.listen(port, function () {
 });
 
 app.get('/', function (require, response) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    // response.sendFile('dist/index.html')
+    response.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 app.get('/test', function (require, response) {
-    res.send(mockAPIResponse)
+    response.send(mockAPIResponse)
 })
 
 // POST method route
 app.post('/languageprocess', async (require, response) => {
     const keys = `${api_id}${api_key}&url=${txt}&lang=${lang}`;
     const url = require.body.data;
-    const response = await fetch(keys);
+    // const response = await fetch(keys);
     try {
         const data = await response.json();
         console.log(data);
